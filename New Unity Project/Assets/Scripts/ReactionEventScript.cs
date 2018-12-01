a4894007650fcda57b1e5b0aafe53179f04366f8
Jonathan <jjjooonnn@gmail.com>
2018-12-01 11:47:47 +0100

    Generic Scene for testing

---
+++ New Unity Project/Assets/Scripts/ReactionEventScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ReactionEventScript : MonoBehaviour {
+    public float threshHold = 0.1f;
+
+    void Start() {
+        //this.GetComponent<Image>().alphaHitTestMinimumThreshold = threshHold;
+    }
+
+    void Update() {
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            Debug.Log("Hovering2");
+        }
+    }
+
+    public void react() {
+        Debug.Log("Reactive");
+    }
+
+    void OnMouseOver() {
+        Debug.Log("Hovering");
+    }
+	
+}
